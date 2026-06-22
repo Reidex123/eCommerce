c3197fdfcb8f8fa38f4120c4ea139e9b40b8afad
Koketso <koketsogaowelwe2@gmail.com>
2026-06-22 13:19:48 +0200

    fix import to the project and define relationships between entities

--- src/main/java/com/API/eCommerce/model/Product.java
+++ src/main/java/com/API/eCommerce/model/Product.java
@@ -5,18 +5,21 @@ import java.util.List;
 
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Data
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String brand;
