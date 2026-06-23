db20f11271ef17f58ed2be591044acd951797008
Koketso <koketsogaowelwe2@gmail.com>
2026-06-23 21:07:13 +0200

    implement product services

---
+++ src/main/java/com/API/eCommerce/service/Product/ProductService.java
@@ -0,0 +1,5 @@
+package com.API.eCommerce.service.Product;
+
+public class ProductService {
+
+}
---
+++ src/main/java/com/API/eCommerce/service/Product/iProductService.java
@@ -0,0 +1,5 @@
+package com.API.eCommerce.service.Product;
+
+public interface iProductService {
+
+}
