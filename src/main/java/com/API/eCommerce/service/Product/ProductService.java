0356f144bef3ef61ae66bb2717ae55c426346415
Koketso <koketsogaowelwe2@gmail.com>
2026-06-25 21:29:57 +0200

    define product repository

--- src/main/java/com/API/eCommerce/service/Product/ProductService.java
+++ src/main/java/com/API/eCommerce/service/Product/ProductService.java
@@ -1,7 +1,10 @@
 package com.API.eCommerce.service.Product;
 
+import org.springframework.stereotype.Service;
+
 import com.API.eCommerce.Repository.productRepository;
 
+@Service
 public class ProductService implements iProductService {
     productRepository ProductRepository;
 }
--- src/main/java/com/API/eCommerce/service/Product/iProductService.java
+++ src/main/java/com/API/eCommerce/service/Product/iProductService.java
@@ -1,5 +1,7 @@
 package com.API.eCommerce.service.Product;
 
+
 public interface iProductService {
+
 
 }
