8eacef39c5400160975ed43f045ff031ce91902f
Koketso <koketsogaowelwe2@gmail.com>
2026-06-26 23:33:10 +0200

    Services: implementing the services for the Product entity

--- src/main/java/com/API/eCommerce/Repository/productRepository.java
+++ src/main/java/com/API/eCommerce/Repository/productRepository.java
@@ -2,7 +2,19 @@ package com.API.eCommerce.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.API.eCommerce.model.Product;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface productRepository extends JpaRepository<Product, Long>{
+
+    @Override
+    Optional<Product> findById(Long id);
+    List<Product> findByBrand(String brand);
+    List<Product> findByName(String name);
+
+    List<Product> findByCategoryAndBrand(String category, String brand);
+
+    List<Product> findByBrandAndName(String brand, String name);
 
 }
--- src/main/java/com/API/eCommerce/service/Product/iProductService.java
+++ src/main/java/com/API/eCommerce/service/Product/iProductService.java
@@ -1,7 +1,11 @@
 package com.API.eCommerce.service.Product;
 
+import java.util.List;
+
+import com.API.eCommerce.model.Product;
 
 public interface iProductService {
 
+    List<Product> getAllProducts();
 
 }
