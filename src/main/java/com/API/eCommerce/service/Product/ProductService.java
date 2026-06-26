b172192d28c67cccec560662b344779f0fc83301
Koketso <koketsogaowelwe2@gmail.com>
2026-06-26 23:33:10 +0200

    Services: implementing the services for the Product entity

--- src/main/java/com/API/eCommerce/service/Product/ProductService.java
+++ src/main/java/com/API/eCommerce/service/Product/ProductService.java
@@ -1,10 +1,23 @@
 package com.API.eCommerce.service.Product;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.API.eCommerce.Repository.productRepository;
+import com.API.eCommerce.model.Product;
 
 @Service
 public class ProductService implements iProductService {
     productRepository ProductRepository;
+
+    public ProductService(productRepository ProductRepository) {
+        this.ProductRepository = ProductRepository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+
+        return this.ProductRepository.findAll();
+    }
 }
